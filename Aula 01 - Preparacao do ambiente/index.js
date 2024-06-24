204e248137f4950f02add2e5dbbadbeff77b8850
Roberto Adrego <roberto.adrego@gmail.com>
2024-06-24 20:36:51 -0300

    new file:   Aula 01 - Preparacao do ambiente/index.js

---
+++ Aula 01 - Preparacao do ambiente/index.js
@@ -0,0 +1,8 @@
+console.log('Meu nome é "Roberto". Estou aprendendo JavaScript às', 10,'da Manhã');  // Este é um comentario de linha
+
+// Este é um comentario no JavaScript
+
+/*
+Este é um comentário de Bloco
+
+*/
